eef53ffc1ca9bc0d6a4a4fda3e666fc97e5ba568
sotonnasa-glitch <sotonnasa@gmail.com>
2026-09-08 18:00:56 +0330

    style: activate premium homepage visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import '@fontsource/plus-jakarta-sans';
 import App from './App.tsx';
 import './index.css';
 import './order-modal-polish.css';
+import './homepage-premium.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
